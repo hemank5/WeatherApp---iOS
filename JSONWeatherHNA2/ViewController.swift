8ffbbeb9e424fe2780d346b78cffb65d9f111f99
Hemank Narula <hemank.narula@gmail.com>
2017-04-29 20:37:07 -0500

    Initial Commit

---
+++ JSONWeatherHNA2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONWeatherHNA2
+//
+//  Created by Hemank Narula on 4/29/17.
+//  Copyright © 2017 Hemank Narula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
